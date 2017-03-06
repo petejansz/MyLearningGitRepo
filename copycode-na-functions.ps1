d1563942a7df0ec908aff0528a4be0d50d27e41c
petejansz@gmail.com <petejansz@gmail.com>
2017-03-05 16:44:06 -0800

    Added an ignore file and another PS1 script.

---
+++ copycode-na-functions.ps1
@@ -0,0 +1,72 @@
+param
+(
+    [string]$script
+)
+
+function envCntrlVar_dependencies([string]$filename) # @dependencies
+{
+	$EnvCtrlVarNamesFile = "$env:USERPROFILE\Documents\CMIPS\Env-ctrl-varnames.txt"
+	$envCntrlVars = cat $EnvCtrlVarNamesFile
+
+	$dependencies = @()
+	
+	foreach ($name in $envCntrlVars)
+	{
+		$name = $name.Trim()
+		
+		if (sls -CaseSensitive -Quiet -pattern $name $filename)
+		{
+			$dependencies += $name
+		}
+	}
+	
+	return $dependencies
+}
+
+function CopyCode_NotAdv_function_dependencies([string]$filename) # @dependencies
+{
+	$CopyCode_NotAdv_function_names = `
+	('SEND_EMAIL                                  '`
+	,'THRESHOLD_WARNING_ENCOUNTERED              '`
+	,'CHECK_FOR_ERROR                            '`
+	,'INITIALIZE_PARMS                           '`
+	,'ORACLE_QUERY_RESULT                        '`
+	,'ORACLE_QUERY_RESULT_ARRAY                  '`
+	,'ORACLE_EXECUTE                             '`
+	,'ORACLE_ETL_SCRIPT                          '`
+	,'COPY_RENAME_FILE_ADV_TO_ADV                '`
+	,'COPY_RENAME_FILE_TO_ADV                    '`
+	,'COPY_RENAME_FILES_TO_ADV                   '`
+	,'GET_TS                                     '`
+	,'COPY_RENAME_FILE_TO_PROCSERVER             '`
+	,'COPY_RENAME_FILE_TO_PROCSERVER_NO_WAIT     '`
+	,'REPORT_SCRIPT_STATUS                       ')
+
+	$dependencies = @()
+
+	foreach ($name in $CopyCode_NotAdv_function_names)
+	{
+		$name = $name.Trim()
+		
+		if (sls -CaseSensitive -Quiet -pattern $name $filename)
+		{
+			$dependencies += $name
+		}
+	}
+	
+	return $dependencies
+}
+
+##### Main
+
+$filename = resolve-path $script
+$dependencies = CopyCode_NotAdv_function_dependencies $filename
+$dependencies += envCntrlVar_dependencies $filename
+
+$report = $script + ': '
+foreach ($name in $dependencies)
+{
+	$report += $name + ' '
+}
+
+$report
